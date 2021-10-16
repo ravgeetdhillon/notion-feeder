a793cda05012cdaf6e24b5e6242311502b5cf8df
ravgeetdhillon <ravgeetdhillon@gmail.com>
2021-10-16 11:36:08 +0530

    feat: added dotfiles, project setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': ['off'],
+    'no-plusplus': ['off'],
+    'no-await-in-loop': ['off'],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require('webpack');
+
+const getSrcPath = (filePath) => {
+  const src = path.resolve(__dirname, 'src');
+  return path.posix.join(src.replace(/\\/g, '/'), filePath);
+};
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  context: __dirname,
+  entry: getSrcPath('/index.js'),
+  stats: { errorDetails: true },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js'],
+  },
+  optimization: {
+    minimize: false,
+  },
+  performance: {
+    hints: false,
+  },
+  watchOptions: {
+    ignored: ['**/dist', '**/node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+            plugins: [
+              [
+                '@babel/plugin-proposal-object-rest-spread',
+                { loose: true, useBuiltIns: true },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new ESLintPlugin(), new webpack.ProgressPlugin()],
+};
